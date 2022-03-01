bd1af869438cd3f03c8b84ec3532cc855a777c58
Florian Rodriguez <florian@negative-network.com>
2022-03-01 14:03:47 +0100

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(2)->create();
+        // \App\Models\Project::factory(10)->create();
+        $users = \App\Models\User::factory(2)->has(\App\Models\Project::factory()->count(5))->create();
     }
 }
